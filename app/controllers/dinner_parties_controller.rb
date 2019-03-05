ac01eec72b9723f7d6d5dfac7d05f4199df2d733
@JSDunn105 <j.s.dunn@icloud.com>
2019-03-05 15:29:54 -0500

    Adding Dinner Party controllers and views to supper club

--- app/controllers/dinner_parties_controller.rb
+++ app/controllers/dinner_parties_controller.rb
@@ -1,2 +1,45 @@
 class DinnerPartiesController < ApplicationController
+
+   def index
+      @dinner_parties = DinnerParty.all
+   end
+
+   def show
+      @dinner_party = DinnerParty.find(params[:id])
+
+   end
+
+   def new
+      @dinner_party = DinnerParty.new()
+
+   end
+
+   def create
+      @dinner_party = DinnerParty.create(dinner_party_params)
+      redirect_to dinner_party_path(@dinner_party.id)
+
+   end
+
+   def edit
+        @dinner_party = Dinner_party.find(params[:id])
+   end
+
+   def update
+        @dinner_party = Dinner_party.update(dinner_party_params)
+        redirect_to dinner_party_path(@dinner_party)
+   end
+
+   def destroy
+        Dinner_party.find(params[:id]).destroy
+        redirect_to dinner_parties_path
+   end
+
+
+   private
+
+   def dinner_party_params
+      params.require(:dinner_party).permit(:user_id, :date, :location, :cuisine, :max_guests, :price_per_guest)
+
+   end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,5 @@ Rails.application.routes.draw do
   resources :users, except: [:index]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  get "/" to: "home#static"
+  get "/", to: "static#home"
 end
