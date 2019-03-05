bcbb61d2aa50f7a1fcb91feec217cc7763217bd4
Dana Simmons <dana@simmons-audio.com>
2019-03-05 12:38:26 -0500

    added log in methods to application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,16 @@
 class ApplicationController < ActionController::Base
+
+    def log_in(user)
+        session[:user_id] = user.id
+    end
+
+    def current_user
+        if session[:user_id]
+            @current_user ||= User.find_by(id: session[:user_id])
+        end
+    end
+
+    def logged_in? #not sure if we need this, but why not
+        !@current_user.nil?
+    end
 end
