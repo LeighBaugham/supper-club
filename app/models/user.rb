c483afdce182fc4bee7ef5d61a4032bd18990881
Leigh Baugham <leigh.baugham@gmail.com>
2019-03-05 15:32:08 -0500

    reviews, dinnerguest, homepage

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,9 @@ class User < ApplicationRecord
     has_many :dinner_parties, through: :dinner_guests
     has_many :hosted_parties, class_name: "DinnerParty"
     has_many :user_reviews, through: :dinner_guests 
+
+    validates :name, presence: true
+    validates :password, length: { minimum: 6}
+    
+
 end
