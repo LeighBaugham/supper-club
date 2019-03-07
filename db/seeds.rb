e483f0c1c393d296c7505ec3875db98650817120
Leigh Baugham <leigh.baugham@gmail.com>
2019-03-07 11:05:52 -0500

    link to review, party column name

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,9 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-scott = User.create(name: "Scott", user_name: "Codedaddy", password_digest: "digest")
-dana = User.create(name: "Dana", user_name: "Dana123", password_digest: "onlyzoll")
-leigh = User.create(name: "Leigh", user_name: "Baugham", password_digest: "apples")
+scott = User.create(name: "Scott", user_name: "Codedaddy", password: "digest")
+dana = User.create(name: "Dana", user_name: "Dana123", password: "onlyzoll")
+leigh = User.create(name: "Leigh", user_name: "Baugham", password: "apples")
 
 party_1 = DinnerParty.create(user_id: 1, location: "flatiron", cuisine: "tex-mex", date: Time.now, max_guests: 8, price_per_guest: 75.00 )
 
